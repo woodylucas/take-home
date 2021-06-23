2414026c55d616acea07c307160689c71db82b74
woodylucas <wlucas0311@gmail.com>
2021-06-23 15:04:31 -0400

    implementing homepage

---
+++ src/components/Header.js
@@ -0,0 +1,79 @@
+import React from "react";
+import styled from "styled-components";
+import PinterestIcon from "@material-ui/icons/Pinterest";
+import IconButton from "@material-ui/core/IconButton";
+
+function Header() {
+  return (
+    <div>
+      <Wrapper>
+        <LogoWrapper>
+          <IconButton>
+            <PinterestIcon />
+          </IconButton>
+        </LogoWrapper>
+        <HomePageButton>
+          <a href="/">Homepage</a>
+        </HomePageButton>
+        <FollowingButton>
+          <a href="/">Following</a>
+        </FollowingButton>
+        {/* <SearchWrapper>
+          <SearchBarWrapper></SearchBarWrapper>
+        </SearchWrapper>
+        <IconsWrapper></IconsWrapper> */}
+      </Wrapper>
+    </div>
+  );
+}
+
+export default Header;
+
+const Wrapper = styled.div`
+  display: flex;
+  align-items: center;
+  height: 56px;
+  padding: 12px 4px 4px 16px
+  background-color: white; 
+  color: black;
+`;
+
+const LogoWrapper = styled.div`
+  .MuiSvgIcon-root {
+    color: #e60023;
+    font-size: 32px;
+    cursor: pointer;
+  }
+`;
+
+const HomePageButton = styled.div`
+  display: flex;
+  height: 48px;
+  min-width: 123px;
+  align-items: center;
+  justify-content: center;
+  border-radius: 24px;
+  cursor: pointer;
+  background-color: rgb(17, 17, 17);
+  a {
+    text-decoration: none;
+    color: white;
+    font-weight: 700;
+  }
+`;
+
+const FollowingButton = styled.div`
+  display: flex;
+  height: 48px;
+  min-width: 123px;
+  align-items: center;
+  justify-content: center;
+  border-radius: 24px;
+  cursor: pointer;
+  background-color: white;
+  a {
+    text-decoration: none;
+    color: black;
+    font-weight: 700;
+  }
+`;
